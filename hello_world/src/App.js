4ce5605e5657247739ba600c70f33ef7c2bebb95
rutiksanap44 <99959986+rutiksanap44@users.noreply.github.com>
2023-04-09 15:01:06 +0530

    Sub Header Added

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Products from './components/Products/Products'
 import Header from './components/Layout/Header';
+import SubHeader from './components/Layout/SubHeader';
 const App = () => {
   return (
     <div className='app'>
     <Header></Header>
+    <SubHeader></SubHeader>
     <Products/>
     </div>
   );
