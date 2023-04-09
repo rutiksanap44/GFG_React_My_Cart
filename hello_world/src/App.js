41928e60d9900eb761bca6bb36423c94856c12e6
rutiksanap44 <99959986+rutiksanap44@users.noreply.github.com>
2023-04-09 14:54:29 +0530

    Header Added

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Products from './components/Products/Products'
+import Header from './components/Layout/Header';
 const App = () => {
   return (
-    <div>
+    <div className='app'>
+    <Header></Header>
     <Products/>
     </div>
   );
